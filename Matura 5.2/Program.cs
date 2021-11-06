128ada5236ffd730c9f94571c9460395be6b5f90
Krzakooo <55135961+Krzakooo@users.noreply.github.com>
2021-11-06 12:57:05 +0100

    Add project files.

---
+++ Matura 5.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Matura_5._2
+{
+    class Program
+    {
+        static String[][] readFile()
+        {
+            List<String[]> list = new List<String[]>();
+            using (StreamReader reader = new StreamReader("pogoda.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    String line = reader.ReadLine();
+                    String[] values = line.Split(';');
+                    list.Add(values);
+                }
+            }
+            return list.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
